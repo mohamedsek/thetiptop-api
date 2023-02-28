c7cc60a44c0402d8675533f27a36140cfbb259fc
mohamedsek <mohamed.sekkoume@gmail.com>
2023-02-28 20:59:34 +0100

    init spring boot app

---
+++ src/main/java/fr/thetiptop/app/TheTipTopAppApplication.java
@@ -0,0 +1,13 @@
+package fr.thetiptop.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TheTipTopAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TheTipTopAppApplication.class, args);
+	}
+
+}
