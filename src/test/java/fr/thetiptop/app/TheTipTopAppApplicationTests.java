c9ba08e0a6b1f4b386efd0e326cb0004677e3afb
mohamedsek <mohamed.sekkoume@gmail.com>
2023-03-02 15:21:56 +0100

    add mock test

--- src/test/java/fr/thetiptop/app/TheTipTopAppApplicationTests.java
+++ src/test/java/fr/thetiptop/app/TheTipTopAppApplicationTests.java
@@ -1,5 +1,6 @@
 package fr.thetiptop.app;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -9,5 +10,8 @@ class TheTipTopAppApplicationTests {
 	@Test
 	void contextLoads() {
 	}
-
+	@Test
+	void testMyAppLogic() {
+		Assertions.assertTrue(1==1, "1 equals 1");
+	}
 }
