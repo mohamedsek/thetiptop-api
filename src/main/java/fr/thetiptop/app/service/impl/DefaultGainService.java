580ad0b84d582ecdde5b0cc20b192b17319de232
mohamedsek <mohamed.sekkoume@gmail.com>
2023-03-05 08:42:00 +0100

    assign-gain : fix/ bypass block cas  dist exact

--- src/main/java/fr/thetiptop/app/service/impl/DefaultGainService.java
+++ src/main/java/fr/thetiptop/app/service/impl/DefaultGainService.java
@@ -112,6 +112,12 @@ public class DefaultGainService implements GainService {
                 .filter(gainModel -> !overUsedGainIds.contains(gainModel.getId()) && gainModel.getChance()>0 )
                 .collect(Collectors.toList());
 
+        if (CollectionUtils.isEmpty(availableGains)) {
+            availableGains = gains.stream()
+                    .filter(gainModel -> overUsedGainIds.contains(gainModel.getId()) && gainModel.getChance()>0 )
+                    .collect(Collectors.toList());
+        }
+
         logger.debug(String.format("Current available gains to assign '%s'", String.join(",", availableGains.stream().map(gain -> gain.getTitle()).collect(Collectors.toList()))));
 
         return availableGains;
