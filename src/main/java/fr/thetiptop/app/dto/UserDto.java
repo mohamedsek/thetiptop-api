e915d778444186609046c31ce5a61bd19704d5d0
mohamedsek <mohamed.sekkoume@gmail.com>
2023-03-05 23:04:50 +0100

    feature/jackpot-winner: create UserDto

---
+++ src/main/java/fr/thetiptop/app/dto/UserDto.java
@@ -0,0 +1,24 @@
+package fr.thetiptop.app.dto;
+
+public class UserDto {
+
+    protected String fullname;
+    protected String email;
+
+    public String getFullname() {
+        return fullname;
+    }
+
+    public void setFullname(String fullname) {
+        this.fullname = fullname;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+}
