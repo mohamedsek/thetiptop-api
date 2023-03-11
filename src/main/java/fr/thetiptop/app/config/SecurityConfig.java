6371643979b964abcaccfbc483d7c6056d768799
atimzit <atimzit@gmail.com>
2023-03-11 20:12:59 +0100

    security: Secure acces to customer endpoints by ROLE_CUSTOMER

--- src/main/java/fr/thetiptop/app/config/SecurityConfig.java
+++ src/main/java/fr/thetiptop/app/config/SecurityConfig.java
@@ -58,6 +58,8 @@ public class SecurityConfig {
                 .httpBasic().disable()
                 .authorizeRequests()
                 .requestMatchers("/tickets").hasRole(Constants.Roles.CHECKOUT_MACHINE)
+                .requestMatchers("/tickets/gain").hasRole(Constants.Roles.CUSTOMER)
+                .requestMatchers("/tickets/user").hasRole(Constants.Roles.CUSTOMER)
                 .requestMatchers("/tickets/jackpot").hasRole(Constants.Roles.ADMIN)
                 .requestMatchers("/gains/stats").hasRole(Constants.Roles.ADMIN)
                 .requestMatchers("/auth/registeradmin").hasRole(Constants.Roles.ADMIN)
