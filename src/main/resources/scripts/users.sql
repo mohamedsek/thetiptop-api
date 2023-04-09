204e0fb6f8cb7f6ed1afe93160f3e7036de7bdb1
atimzit <atimzit@gmail.com>
2023-04-09 16:55:02 +0200

    Test: Participation e2e test

---
+++ src/main/resources/scripts/users.sql
@@ -0,0 +1,8 @@
+
+
+INSERT INTO game_contest_test.`user`
+(dtype, id, email, enabled, first_name, last_name, password, uid, role_id)
+VALUES('Client', 1, 'customer@domain.com', 1, 'customerF', 'customerL', '{bcrypt}$2a$10$QQ6EYDbj3MESyDRQz4LgP.J/A.jbuvha.9obvmw3bprl3VsJYcpUW', '850a1fb5-b4cf-4f1e-b52e-080f81cc4b9e', 1);
+
+
+
